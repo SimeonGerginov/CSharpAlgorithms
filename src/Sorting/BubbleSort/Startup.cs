70362b931b562a8722b8fde4c2b9f7d7b777a89a
SimeonGerginov <srgerginov@uni-sofia.bg>
2018-05-15 16:16:34 +0300

    Added BubbleSort implementation

---
+++ src/Sorting/BubbleSort/Startup.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Linq;
+
+namespace BubbleSort
+{
+    public class Startup
+    {
+        public static void Main()
+        {
+            Console.WriteLine("Please enter the array of numbers:");
+
+            int[] arrayOfNumbers = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+            Console.WriteLine();
+            BubbleSortAlgorithm(arrayOfNumbers);
+
+            Console.WriteLine("Sorted array:");
+            Console.WriteLine(string.Join(" ", arrayOfNumbers));
+        }
+
+        private static void BubbleSortAlgorithm(int[] arrayOfNumbers)
+        {
+            bool swapped;
+            for (int i = 0; i < arrayOfNumbers.Length - 1; i++)
+            {
+                swapped = false;
+                for (int j = 0; j < arrayOfNumbers.Length - i - 1; j++)
+                {
+                    if (arrayOfNumbers[j] > arrayOfNumbers[j + 1])
+                    {
+                        int temp = arrayOfNumbers[j];
+                        arrayOfNumbers[j] = arrayOfNumbers[j + 1];
+                        arrayOfNumbers[j + 1] = temp;
+                        swapped = true;
+                    }
+                }
+
+                if (!swapped)
+                {
+                    break;
+                }
+            }
+        }
+    }
+}
