cd4dd18ee7de85966dce01e315be7f69d8775215
SimeonGerginov <srgerginov@uni-sofia.bg>
2018-05-15 16:39:59 +0300

    Added RecursiveBubbleSort implementation

---
+++ src/Sorting/RecursiveBubbleSort/Startup.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Linq;
+
+namespace RecursiveBubbleSort
+{
+    public class Startup
+    {
+        public static void Main()
+        {
+            Console.WriteLine("Please enter the array of numbers:");
+
+            int[] arrayOfNumbers = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+            int arrayLength = arrayOfNumbers.Length;
+            Console.WriteLine();
+            RecursiveBubbleSortAlgorithm(arrayOfNumbers, arrayLength);
+
+            Console.WriteLine("Sorted array:");
+            Console.WriteLine(string.Join(" ", arrayOfNumbers));
+        }
+
+        private static void RecursiveBubbleSortAlgorithm(int[] arrayOfNumbers, int arrayLength)
+        {
+            if (arrayLength == 1)
+            {
+                return;
+            }
+
+            for (int i = 0; i < arrayLength - 1; i++)
+            {
+                if (arrayOfNumbers[i] > arrayOfNumbers[i + 1])
+                {
+                    int temp = arrayOfNumbers[i];
+                    arrayOfNumbers[i] = arrayOfNumbers[i + 1];
+                    arrayOfNumbers[i + 1] = temp;
+                }
+            }
+
+            RecursiveBubbleSortAlgorithm(arrayOfNumbers, arrayLength - 1);
+        }
+    }
+}
