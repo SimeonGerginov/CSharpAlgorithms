3076e19d64fed66520667daa8a843794b8917870
SimeonGerginov <srgerginov@uni-sofia.bg>
2018-06-20 22:43:09 +0300

    Added UniqueCharacters and CheckPermutation

---
+++ src/ArraysAndStrings/UniqueCharacters/Startup.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace UniqueCharacters
+{
+    public class Startup
+    {
+        private const int CharactersCount = 128;
+
+        public static void Main()
+        {
+            var charactersString = Console.ReadLine();
+
+            bool hasAllUniqueCharacters = HasAllUniqueCharacters(charactersString);
+
+            Console.WriteLine(hasAllUniqueCharacters);
+        }
+
+        /// <summary>
+        /// Algorithm to determine if a string has all unique characters.
+        /// </summary>
+        private static bool HasAllUniqueCharacters(string charactersString)
+        {
+            var charactersSet = new bool[CharactersCount];
+
+            foreach (var character in charactersString)
+            {
+                if (charactersSet[character])
+                {
+                    return false;
+                }
+
+                charactersSet[character] = true;
+            }
+
+            return true;
+        }
+    }
+}
