dcaecf8da001589e0ad9078823b30c55b93d6700
SimeonGerginov <srgerginov@uni-sofia.bg>
2018-06-20 22:59:48 +0300

    Added URLify

---
+++ src/ArraysAndStrings/URLify/Startup.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Text;
+
+namespace URLify
+{
+    public class Startup
+    {
+        public static void Main()
+        {
+            var charactersString = Console.ReadLine().TrimEnd(new [] {' '});
+
+            var replacedString = ReplacedString(charactersString);
+
+            Console.WriteLine(replacedString);
+        }
+
+        /// <summary>
+        /// Method to replace all spaces in a string with '%20'.
+        /// </summary>
+        private static string ReplacedString(string charactersString)
+        {
+            var sb = new StringBuilder();
+
+            foreach (var character in charactersString)
+            {
+                if (character != ' ')
+                {
+                    sb.Append(character);
+                }
+                else
+                {
+                    sb.Append("%20");
+                }
+            }
+
+            return sb.ToString();
+        }
+    }
+}
