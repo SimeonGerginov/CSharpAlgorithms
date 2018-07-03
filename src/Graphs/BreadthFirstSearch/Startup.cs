9641931d4bb057739ee39ef36d9c0bdeac090354
SimeonGerginov <srgerginov@uni-sofia.bg>
2018-07-03 16:35:51 +0300

    Added DepthFirstSearch and BreadthFirstSearch implementations

---
+++ src/Graphs/BreadthFirstSearch/Startup.cs
@@ -0,0 +1,22 @@
+﻿namespace BreadthFirstSearch
+{
+    public class Startup
+    {
+        public static void Main()
+        {
+            var numberOfVertices = 6;
+            var matrix = new int[,]
+            {
+                { 0, 1, 0, 0, 1, 0 },
+                { 1, 0, 1, 0, 0, 1 },
+                { 0, 0, 0, 1, 0, 0 },
+                { 0, 1, 0, 0, 0, 1 },
+                { 1, 0, 0, 1, 0, 1 },
+                { 1, 0, 1, 0, 1, 0 }
+            };
+
+            var graph = new Graph(numberOfVertices, matrix);
+            graph.BreadthFirstSearch(0);
+        }
+    }
+}
